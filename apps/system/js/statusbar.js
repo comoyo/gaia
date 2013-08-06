200b8a556f33abb10e2ff47bb7a172ab5fb39534
Sergi Mansilla <sergi.mansilla@gmail.com>
2013-08-06 18:01:01 +0200

    Prototype isolating the lockscreen as a separate application

--- apps/system/js/statusbar.js
+++ apps/system/js/statusbar.js
@@ -3,61 +3,6 @@
 
 'use strict';
 
-/**
- * Creates an object used for refreshing the clock UI element. Handles all
- * related timer manipulation (start/stop/cancel).
- */
-function Clock() {
-  /** One-shot timer used to refresh the clock at a minute's turn */
-  this.timeoutID = null;
-
-  /** Timer used to refresh the clock every minute */
-  this.timerID = null;
-
-  /**
-   * Start the timer used to refresh the clock, will call the specified
-   * callback at every timer tick to refresh the UI. The callback used to
-   * refresh the UI will also be called immediately to ensure the UI is
-   * consistent.
-   *
-   * @param {Function} refresh Function used to refresh the UI at every timer
-   *        tick, should accept a date object as its only argument.
-   */
-  this.start = function cl_start(refresh) {
-    var date = new Date();
-    var self = this;
-
-    refresh(date);
-
-    if (this.timeoutID == null) {
-      this.timeoutID = window.setTimeout(function cl_setClockInterval() {
-        refresh(new Date());
-
-        if (self.timerID == null) {
-          self.timerID = window.setInterval(function cl_clockInterval() {
-            refresh(new Date());
-          }, 60000);
-        }
-      }, (60 - date.getSeconds()) * 1000);
-    }
-  };
-
-  /**
-   * Stops the timer used to refresh the clock
-   */
-  this.stop = function cl_stop() {
-    if (this.timeoutID != null) {
-      window.clearTimeout(this.timeoutID);
-      this.timeoutID = null;
-    }
-
-    if (this.timerID != null) {
-      window.clearInterval(this.timerID);
-      this.timerID = null;
-    }
-  };
-}
-
 var StatusBar = {
   /* all elements that are children nodes of the status bar */
   ELEMENTS: ['notification', 'emergency-cb-notification', 'time',
